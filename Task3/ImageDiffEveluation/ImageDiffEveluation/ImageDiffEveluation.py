af1e80ca6e9d95ea03bd9e8091e1e5f2eaaec849
Kevan Rahav <kevan.or@gmail.com>
2018-12-06 12:44:38 -0700

    Task 3 Comments

--- Task3/ImageDiffEveluation/ImageDiffEveluation/ImageDiffEveluation.py
+++ Task3/ImageDiffEveluation/ImageDiffEveluation/ImageDiffEveluation.py
@@ -1,26 +1,11 @@
-import imageio
+#The program return an RMS value, which depends on the images, the more different they are, the higher the value; 
 from PIL import Image
 from PIL import ImageChops
 import math
 #from skimage.measure import compare_ssim
 
-#im = imageio.imread('imageio:chelsea.png')
-#print(im.shape)
-
-#from PIL import Image
 import glob
-image_list = []
-for filename in glob.glob('../../test_data/2D_registration/*.png'): #assuming gif
-    #im=Image.open(filename)
-    im=imageio.imread(filename)
-    image_list.append(im)
 
-#print(image_list[0].shape)
-#print(image_list[1].shape)
-
-
-#im1 = Image.open("../../test_data/2D_registration/foodtest0.jpg")
-#im2 = Image.open("../../test_data/2D_registration/foodtest1.jpg")
 
 im1 = Image.open("../../test_data/diff_test/test_image_1.png")
 im2 = Image.open("../../test_data/diff_test/test_image_2.png")
