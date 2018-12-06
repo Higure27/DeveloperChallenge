51372dcc748e086843d95ddabb8ff924dc0ce53b
Kevan Rahav <kevan.or@gmail.com>
2018-12-06 13:01:34 -0700

    Added comments to bonus task1

--- BonusTask1/Dependency/Dependency/Program.cs
+++ BonusTask1/Dependency/Dependency/Program.cs
@@ -11,16 +11,20 @@ namespace Dependency
     {
         static void Main(string[] args)
         {
+            //Get list of all projects
             string givenProjectString = Console.ReadLine();
             givenProjectString =Regex.Replace(givenProjectString, @"projects:", "");
             givenProjectString = givenProjectString.Replace(" ", "");
             List<string> givenProjectsList = givenProjectString.Split(',').ToList();
 
+            //Get string of all dependencis 
             string dependencyString = Console.ReadLine();
             MatchCollection matchList = Regex.Matches(dependencyString, @"\(([^)]*)\)");
             List<string> dependencyPairList = matchList.Cast<Match>().Select(match => match.Value).ToList();
+            //key: Dependent, value: list of dependees
             Dictionary<string, List<string>> allDependecies = new Dictionary<string, List<string>>();
 
+            //Put dependencis into dictionary
             foreach(string pair in dependencyPairList)
             {
                 string currentPair = pair.Replace(" ","");
@@ -30,6 +34,11 @@ namespace Dependency
             }
         }
 
+        /// <summary>
+        /// Gets dependency ffrom the string and puts into dependencies dictionary. 
+        /// </summary>
+        /// <param name="currentPair"></param>
+        /// <param name="allDependecies"></param>
         private static void GetDependencyFromString(string currentPair, Dictionary<string, List<string>> allDependecies)
         {
             string[] dependeeDependent = currentPair.Split(',');
